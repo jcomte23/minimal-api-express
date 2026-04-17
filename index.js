a304765e246cb1b157f6bedae61553de78979a42
Javier Cómbita Tellez <javiercombita2014@gmail.com>
2026-04-17 15:11:42 -0500

    refactor: update deployment workflow, remove ecosystem config, and add port configuration via environment variables

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mysql = require("mysql2/promise");
 
 const app = express();
-const PUERTO = 3000;
+const PUERTO = process.env.PORT || 3000;
 
 app.use(express.json());
 
